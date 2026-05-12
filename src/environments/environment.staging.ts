170ffccd4829859b47618867693dae5b0bed3558
Brandon May <brandonmaykie@gmail.com>
2026-05-12 19:49:53 +0400

    staging config

---
+++ src/environments/environment.staging.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: true,
+  supabaseUrl: 'https://awmzyrblplsibsxdjafl.supabase.co',
+  supabaseAnonKey: 'sb_publishable_VGwPnRRoqx9wLmFwbLfAEw_uSMQPF7X',
+};
